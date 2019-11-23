eea1e4cfe7b594cca258c62fec2d34954af7cdaa
Nick Hartford <skyler0121@gmail.com>
2019-11-22 22:44:12 -0500

    created models

--- ndt_site/pages/models.py
+++ ndt_site/pages/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from towers.models import Towers
 
 # Create your models here.
+class Products(models.Model):
+    productId = models.AutoField(primary_key=True)
+    towerId = models.ForeignKey(Towers, on_delete=models.CASCADE)
+    productName = models.CharField(max_length=100)
+    description = models.TextField()
+    units = models.IntegerField()
+    price = models.IntegerField()
+    comments = models.TextField()
+
+
+class OrderProducts(models.Model):
+    orderId = models.ForeignKey(Products, on_delete=models.CASCADE)
+    productId = models.ForeignKey(Products, on_delete=models.CASCADE)
+    qtyEach = models.IntegerField()
+    comments = models.TextField()
+
+
+class Orders(models.Model):
+    orderId = models.AutoField(primary_key=True)
+    customerId = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    dateOrdered = models.DateTimeField()
+    comments = models.TextField()
+
+
+class Shipping(models.Model):
+    shippingId = models.AutoField(primary_key=True)
+    orderId = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    dateOrdered = models.DateTimeField()
+    comments = models.TextField()
+
+
+class Customers(models.Model):
+    CustomerId = models.AutoField(primary_key=True)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    zipCode = models.CharField(max_length=10)
+    message = models.TextField()
+    review = models.TextField()
+
+
+class PhoneNumbers(models.Model):
+    customerId = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    primaryNumber = models.IntegerField()
+    secondaryNumber = models.IntegerField()
+
+## Dealers class
+class Dealers(models.Model):
+    dealerId = models.AutoField(primary_key=True)
+    dealerName = models.CharField(max_length=20)
+    dealerClass = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    zipCode = models.CharField(max_length=20)
+    phone = models.IntegerField()
+    contactName = models.CharField(max_length=20)
+    has__ordered = models.BooleanField()
--- ndt_site/towers/models.py
+++ ndt_site/towers/models.py
@@ -1,5 +1,54 @@
 from django.db import models
 
+
 # Create your models here.
 class Towers(models.Model):
-    title = models.CharField(max_length=200)
+    FINISHES = (
+        ('A', 'Anodized'),
+        ('PC', 'Powder coated'),
+        ('P', 'Polished'),
+    )
+
+    towerId = models.AutoField(primary_key=True)
+    productId = models.ForeignKey(Product, on_delete=models.CASCADE)
+    boatName = models.CharField(max_length=20)
+    boatModel = models.CharField(max_length=20)
+    style = models.CharField(max_length=20)
+    finish = models.CharField(max_length=2, choices=FINISHES)
+    price = models.IntegerField()
+    description = models.TextField(blank=True)
+
+
+class Biminis(models.Model):
+
+    COLORS = (
+        ('BK', 'Black'),
+        ('RD', 'Red'),
+        ('BL', 'Blue'),
+        ('MR', 'Maroon'),
+        ('BD', 'Burgandy'),
+    )
+
+    biminiId = models.AutoField(primary_key=True)
+    towerId = models.ForeignKey(Towers, on_delete=models.CASCADE)
+    color = models.CharField(max_length=2, choices=COLORS)
+    price = models.IntegerField()
+
+
+class Images(models.Model):
+    imageId = models.AutoField(primary_key=True)
+    towerId = models.ForeignKey(Towers, on_delete=models.CASCADE)
+    profile = models.ImageField()
+    angled = models.ImageField()
+    collapsed = models.ImageField()
+    back = models.ImageField()
+
+
+class ModelYear(models.Model):
+    modelId = models.AutoField(primary_key=True)
+    towerId = models.ForeignKey(Towers, on_delete=models.CASCADE)
+    m__year = models.IntegerField()
+
+
+
+
